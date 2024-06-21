d3d02ee4b88086428085b348fa3facdb88c258ae
ckk914 <ckk9114@naver.com>
2024-06-22 00:12:52 +0900

    주석 수정

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -2,6 +2,7 @@ import React, { createContext } from "react";
 
 // 장바구니에 관련된 상태값들을 관리하는 컨텍스트(=저장소)
 // 매개변수에 전달하는 객체는 상태들을 정의하는 개념
+// ㄴ 인터페이스 처럼 정의하는 곳⭐️
 const CartContext = createContext({
   cartItems: [], // 장바구니 배열
   addItem: (item) => {}, // 장바구니에 추가하는 함수
