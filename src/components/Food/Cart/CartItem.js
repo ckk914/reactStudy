5c191473dbba4ff40730a3eb768aa1f832855289
ckk914 <ckk9114@naver.com>
2024-06-24 09:24:59 +0900

    # 장바구니 아이템 렌더링

---
+++ src/components/Food/Cart/CartItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./CartItem.module.scss";
+
+const CartItem = ({ cart }) => {
+  const { name, price, amount } = cart;
+
+  const {
+    "cart-item": cartItem,
+    summary,
+    price: priceStyle,
+    amount: amountStyle,
+    actions,
+  } = styles;
+  const formatPrice = new Intl.NumberFormat("ko-KR").format(price);
+
+  return (
+    <li className={cartItem}>
+      <div>
+        <h2>{name}</h2>
+        <div className={summary}>
+          <span className={priceStyle}>{formatPrice}</span>
+          <span className={amountStyle}>x {amount}</span>
+        </div>
+      </div>
+      <div className={actions}>
+        <button>−</button>
+        <button>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
--- src/components/Food/Cart/cart.js
+++ src/components/Food/Cart/cart.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Cart.module.scss";
 import CartModal from "./CartModal";
+import CartContext from "../../../store/cart-context";
+import CartItem from "./CartItem";
 
-const DUMMY_CART = [
-  {
-    id: "c1",
-    name: "스시",
-    amount: 2,
-    price: 46000,
-  },
-  {
-    id: "c2",
-    name: "띠드버거",
-    amount: 1,
-    price: 12000,
-  },
-];
+// const DUMMY_CART = [
+//   {
+//     id: "c1",
+//     name: "스시",
+//     amount: 2,
+//     price: 46000,
+//   },
+//   {
+//     id: "c2",
+//     name: "띠드버거",
+//     amount: 1,
+//     price: 12000,
+//   },
+// ];
 const Cart = ({ onClose }) => {
+  const { cartItems } = useContext(CartContext);
   const {
     "cart-items": cartItemStyle,
     total,
@@ -29,8 +32,8 @@ const Cart = ({ onClose }) => {
     <CartModal onClose={onClose}>
       {/* 주문 내역 */}
       <ul className={cartItemStyle}>
-        {DUMMY_CART.map((cartItem) => (
-          <li key={cartItem.id}>{cartItem.name}</li>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cart={cartItem} />
         ))}
       </ul>
       <div className={total}>
