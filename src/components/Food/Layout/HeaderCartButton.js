7a070dce40620d42c1c1d6b7c474b961b19db6ca
ckk914 <ckk9114@naver.com>
2024-06-24 12:41:33 +0900

    # 카트 담기 이팩트

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,12 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.scss";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = ({ onShow }) => {
+  //bump 애니메이션을 제어하는 상태 변수
+  const [isBump, setIsBump] = useState(false);
+  //장바구니 배열 가져오기
   const { cartItems } = useContext(CartContext);
-  // 리덕스 사용시
+
   //curNumber에 누적해서 더해줌
+  // reduce가 cartItems반복해서 순회
   const totalAmount = cartItems.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
@@ -19,15 +23,31 @@ const HeaderCartButton = ({ onShow }) => {
   //   return totalAmount;
   // };
 
-  const { button, icon, badge } = styles;
+  const { button, icon, badge, bump } = styles;
+
+  useEffect(() => {
+    // console.log("useEffect 실행");
+    if (cartItems.length === 0) return;
+    setIsBump(true);
+    //애니메이션 실행 후 클래스 제거
+    // 추후 재시작을 하기 위함!
+    const timer = setTimeout(() => {
+      setIsBump(false);
+    }, 300);
+    // 애니메이션 실행 후 클래스 제거
+    // 추후 재시작을 하기 위함!
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartItems]);
 
   return (
-    <button className={button} onClick={onShow}>
+    <button className={`${button} ${isBump ? bump : ""}`} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
       <span>My Cart</span>
-      <span className={badge}>{calcTotalAmount()}</span>
+      <span className={badge}>{totalAmount}</span>
     </button>
   );
 };
